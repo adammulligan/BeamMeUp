d0bdb37186514a9ec7bc9834d8480d492c6eec40
Adam Mulligan <adammulligan@gmail.com>
2013-03-29 20:03:54 +0000

    Changed unicorn port

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,6 +7,8 @@ log_file = "#{rails_root}/log/unicorn.log"
 err_log = "#{rails_root}/log/unicorn_error.log"
 old_pid = pid_file + '.oldbin'
 
+listen 6000
+
 timeout 30
 worker_processes 2 # increase or decrease
 listen socket_file, :backlog => 1024
